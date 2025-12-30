adc01226a9f5ab27e4a7fdeed794b7a8882718e1
AR-26710 <Eden1031@qq.com>
2025-12-31 04:32:46 +0800

    feat(seo): 添加ogSiteName元数据

--- src/data/seo.ts
+++ src/data/seo.ts
@@ -6,5 +6,6 @@ export default {
   ogDescription: '呀哈喽的个人名片👋',
   ogUrl: 'https://houxiongxiong.com',
   ogImage: 'https://oss.houxiongxiong.icu/home/home-bg.jpg',
+  ogSiteName: '呀哈喽',
   twitterCard: 'summary_large_image',
 };
