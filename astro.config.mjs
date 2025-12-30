44a5dbf9fb072b1fc9d69bc9ab952da0d3f4d630
AR-26710 <Eden1031@qq.com>
2025-12-30 20:01:10 +0800

    feat(PWA): 添加安装提示组件

--- astro.config.mjs
+++ astro.config.mjs
@@ -43,6 +43,8 @@ export default defineConfig({
         workbox: {
           maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
           globPatterns: ['**/*.{js,css,html,ico,png,jpg,jpeg,svg,woff,woff2,ttf,eot}'],
+          navigateFallback: '/index.html',
+          navigateFallbackDenylist: [/^\/api\/.*/],
           runtimeCaching: [
             {
               urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
