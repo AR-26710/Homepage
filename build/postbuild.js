63cf66ffcd7b303b0d3cc8aab3a5845d838231e1
AR-26710 <Eden1031@qq.com>
2025-12-31 06:00:05 +0800

    refactor: 移除未使用的分析跟踪代码

--- build/postbuild.js
+++ build/postbuild.js
@@ -11,15 +11,6 @@ const indexHtml = fs.readFileSync(indexHtmlPath, "utf-8");
 const regex = /\/_astro\/(.*)\.css/;
 let updatedIndexHtml = indexHtml.replace(regex, `${site}/_astro/$1.css`);
 
-// change gtag by mode
-const gtags = {
-  moe: "EPS8XLSB6V",
-  cn: "W8NZX8HDXQ",
-  top: "K3RTHCN381",
-};
-
-updatedIndexHtml = updatedIndexHtml.replaceAll(gtags["moe"], gtags[mode]);
-
 // 非cn场景删除备案信息
 if(mode !== "cn") {
   const beian = /\<div class="beian".*?京.*号.*京.*号.*\<\/div\>/;
